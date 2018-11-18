8a842cb153f3c54782ae5bce31fc5144d7b90872
linzhouzhi <490664646@qq.com>
2018-11-18 22:23:22 +0800

    config

--- src/main/resources/public/js/admin/config.js
+++ src/main/resources/public/js/admin/config.js
@@ -1,15 +1,16 @@
 /**
  * Created by lzz on 2018/10/4.
  */
-window.domain="http://localhost:8182/";
+//window.domain="http://localhost:8182/";
+window.domain="http://39.105.114.48:8182/";
 $(function(){
     autoGetUser(function(obj){
         window.user = obj.res;
         var username = obj.res["username"];
         window.database=obj.res["databaseName"];
-        window.imageurl="http://localhost:8182/package/pack/" + window.database + "/";
-        window.musicurl="http://localhost:8182/package/pack/" + window.database + "/";
-        window.videourl="http://localhost:8182/package/pack/" + window.database + "/";
+        window.imageurl=window.domain + "package/pack/" + window.database + "/";
+        window.musicurl=window.domain + "package/pack/" + window.database + "/";
+        window.videourl=window.domain + "package/pack/" + window.database + "/";
 
         $("#username").text(username);
 
